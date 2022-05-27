0cf19595250c32948d54b5c5eca1031897ee79b2
mgroth0 <mgroth49@gmail.com>
2022-05-27 16:56:51 -0400

    autocommit

--- src/main/kotlin/matt/exec/cmd/cmd.kt
+++ src/main/kotlin/matt/exec/cmd/cmd.kt
@@ -1,61 +1,66 @@
 package matt.exec.cmd
 
 import matt.exec.app.App
+import matt.exec.cmd.CommandLineApp.Companion.exitCommands
 import matt.kjlib.lang.whileTrue
 import matt.kjlib.str.taball
 import matt.klibexport.klibexport.go
 
 class CommandLineApp(
-    val welcomeMessage: String? = null,
-    mainPrompt: String,
-    args: Array<String> = arrayOf(),
-    private val cfg: (() -> Unit)? = null,
-    private val cmdDSL: CommandLineApp.() -> Unit
-) : App(args) {
-    var stayAlive = true
-    fun start(
-        shutdown: (App.() -> Unit)? = null,
-        consumeShutdown: (App.() -> Unit)? = null,
-    ) {
-        welcomeMessage?.go { println(it) }
-        main(
-            shutdown = shutdown, consumeShutdown = consumeShutdown, cfg=cfg
-        )
-        cmdDSL.invoke(this)
-        whileTrue { rootInputPoint.run() }
-    }
+  val welcomeMessage: String? = null,
+  mainPrompt: String,
+  args: Array<String> = arrayOf(),
+  private val cfg: (()->Unit)? = null,
+  private val cmdDSL: CommandLineApp.()->Unit
+): App(args) {
 
-    fun String.does(op: () -> Unit) {
-        rootActions[this] = op
-    }
+  fun start(
+	shutdown: (App.()->Unit)? = null,
+	consumeShutdown: (App.()->Unit)? = null,
+  ) {
+	welcomeMessage?.go { println(it) }
+	main(
+	  shutdown = shutdown, consumeShutdown = consumeShutdown, cfg = cfg
+	)
+	cmdDSL.invoke(this)
+	whileTrue { rootInputPoint.run() }
+  }
 
-    fun acceptAny(op: (String) -> Unit) {
-        rootInputPoint.acceptAnyFun = op
-    }
+  fun String.does(op: ()->Unit) {
+	rootActions[this] = op
+  }
 
-    private val rootActions = mutableMapOf<String, () -> Unit>()
-    private val rootInputPoint = InputPoint(mainPrompt, rootActions)
+  fun acceptAny(op: (String)->Unit) {
+	rootInputPoint.acceptAnyFun = op
+  }
+
+  private val rootActions = mutableMapOf<String, ()->Unit>()
+  private val rootInputPoint = InputPoint(mainPrompt, rootActions)
+
+  companion object {
+	val exitCommands = listOf("q", "exit", "quit")
+  }
 }
 
-val exitCommands = listOf("q", "exit", "quit")
 
-class InputPoint(val prompt: String, val actions: Map<String, () -> Unit>) {
-    var acceptAnyFun: ((String) -> Unit)? = null
-    fun run(): Boolean {
-        print(prompt)
-        val command = readLine()!!
-        if (command in exitCommands) {
-            return false
-        }
-        if (acceptAnyFun != null) {
-            require(actions.isEmpty())
-            acceptAnyFun!!(command)
-        } else {
-            actions[command]?.invoke() ?: run {
-                println("valid commands:")
-                taball(actions.keys)
-            }
-        }
-        return true
-    }
+class InputPoint(
+  private val prompt: String,
+  private val actions: Map<String, ()->Unit>
+) {
+  var acceptAnyFun: ((String)->Unit)? = null
+  fun run(): Boolean {
+	print(prompt)
+	val command = readLine()!!
+	if (command in exitCommands) return false
+	if (acceptAnyFun != null) {
+	  require(actions.isEmpty())
+	  acceptAnyFun!!(command)
+	} else {
+	  actions[command]?.invoke() ?: run {
+		println("valid commands:")
+		taball(actions.keys + exitCommands)
+	  }
+	}
+	return true
+  }
 }
