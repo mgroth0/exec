beeb2f9cf4e4bc6bc314615921b3a9d08ef986a9
mgroth0 <mgroth49@gmail.com>
2022-06-12 13:40:05 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
   api(projects.kj.kjlib)
   implementation(projects.kj.json)
   api(projects.kj.auto)
-  api(projects.kj.kjlib.socket)
+  api(projects.kj.kjlib.kjlibSocket)
   implementation(libs.kotlinx.coroutines)
   implementation(projects.kj.reflect)
   implementation(projects.kj.async)
